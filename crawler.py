eb31c3e02476197012066b61234b7d8ce25948e9
ansontsang1112 <tch@s.hypernite.com>
2022-08-06 00:34:13 +0800

    Added General ID and Category (20220806)

--- crawler.py
+++ crawler.py
@@ -5,6 +5,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from datetime import datetime
+from string import digits
 
 from redis.commands.json.path import Path
 
@@ -49,8 +50,11 @@ def crawler():
             articles[count] = page
 
             if main.ENABLE_REDIS and client is not None:
+                client.hset(count, "id", page.get("id"))
+                client.hset(count, "category", page.get("category"))
                 client.hset(count, "content", page.get("content"))
                 client.hset(count, "img", page.get("img"))
+
         count += 1
 
     print("\n靠北麥塊爬蟲系統 | All article fetched successfully")
@@ -71,19 +75,32 @@ def fetch_page(post_number: int):
 
     soup = BeautifulSoup(web_page, 'html.parser')
     tag_content_search = soup.find("meta", property="og:description")
+    title_search = soup.find("meta", property="og:title")
+
+    def get_digit(title):
+        d = ""
+        for w in title:
+            if w.isdigit():
+                d = d + w
+        return d
 
     formatted_content = str(tag_content_search).replace('<meta content="麥塊匿名發文平台 » cbmc.club › 檢視文章 • ', '').replace(
         '" property="og:description"/>', '')
+
+    formatted_type = str(title_search).replace('<meta content="麥塊匿名發文平台 » cbmc.club › 檢視文章 • ', '').replace(
+        '" property="og:title"/>', '').translate(str.maketrans('', '', digits))
+    formatted_id_by_type = get_digit(str(title_search))
 
     if stop_code(formatted_content):
         return 0
 
     try:
-        img = str(soup.find_all("img", class_="image_max_width")[1]).split(" ")[2].replace('src="', '').replace('"/>','')
+        img = str(soup.find_all("img", class_="image_max_width")[1]).split(" ")[2].replace('src="', '').replace('"/>',
+                                                                                                                '')
     except IndexError:
         img = "None"
 
-    return {"content": formatted_content, "img": img}
+    return {"id": formatted_id_by_type, "category": formatted_type, "content": formatted_content, "img": img}
 
 
 def stop_code(content: str):
